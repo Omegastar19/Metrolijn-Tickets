186aadafbcbc7c3f1a189ca484fd1fdfa91b5c8c
Omegastar19 <117172527+Omegastar19@users.noreply.github.com>
2024-03-28 14:44:05 +0100

    Update PDFGenerator.py

--- PDFGenerator.py
+++ PDFGenerator.py
@@ -1,17 +1,23 @@
 class PDFGenerator:
-    def save_ticket_information_as_pdf(self, age, ticket_type, price, num_zones=None, filename_prefix="ticket"):
-        date_str = datetime.now().strftime("%Y-%m-%d")
-        filename = f"{filename_prefix}_{date_str}.pdf"
+    def __init__(self):
+        pass
 
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font("Arial", size=12)
-        pdf.cell(200, 10, txt="Ticket Information", ln=True, align="C")
-        pdf.cell(200, 10, txt="", ln=True)
-        pdf.cell(200, 10, txt=f"Age: {age}", ln=True)
-        pdf.cell(200, 10, txt=f"Ticket Type: {ticket_type.capitalize()}", ln=True)
-        if ticket_type == 'single':
-            pdf.cell(200, 10, txt=f"Number of Zones: {num_zones}", ln=True)
-        pdf.cell(200, 10, txt=f"The price of your {ticket_type} ticket is: {price:.2f} euro.", ln=True)
-        pdf.cell(200, 10, txt="Ticket generated on: " + datetime.now().strftime("%Y-%m-%d"), ln=True)
-        pdf.output(filename)
+    def save_ticket_information_as_pdf(self, age, ticket_type, price, num_zones=None, filename_prefix="ticket"):
+        generate_pdf = input("Do you want to generate a PDF for this ticket? (yes/no): ")
+        if generate_pdf.lower() == 'yes':
+            date_str = datetime.now().strftime("%Y-%m-%d")
+            filename = f"{filename_prefix}_{date_str}.pdf"
+
+            pdf = FPDF()
+            pdf.add_page()
+            pdf.set_font("Arial", size=12)
+            pdf.cell(200, 10, txt="Ticket Information", ln=True, align="C")
+            pdf.cell(200, 10, txt="", ln=True)
+            pdf.cell(200, 10, txt=f"Age: {age}", ln=True)
+            pdf.cell(200, 10, txt=f"Ticket Type: {ticket_type.capitalize()}", ln=True)
+            if ticket_type == 'single':
+                pdf.cell(200, 10, txt=f"Number of Zones: {num_zones}", ln=True)
+            pdf.cell(200, 10, txt=f"The price of your {ticket_type} ticket is: {price:.2f} euro.", ln=True)
+            pdf.cell(200, 10, txt="Ticket generated on: " + datetime.now().strftime("%Y-%m-%d"), ln=True)
+            pdf.output(filename)
+            print("PDF generated:", filename)
